abd046805363cbac75bda8699e88aa4d88c83f3c
Vogel <vogel@github.com>
2018-10-03 17:16:39 -0300

    Commits

--- src/Library/SendWhatsApp.cs
+++ src/Library/SendWhatsApp.cs
@@ -6,7 +6,8 @@ namespace Library {
         const string accountSid = "ACfbd57f50e199a28eac49de4cc4acfb8a";
         const string authToken = "81795970808380267013bf04070a5936";
         WhatsAppApi whatsApp = new WhatsAppApi (accountSid, authToken);
-        public void Send (Message messageW) {
+        public void Send (Message messageW) 
+        {
             whatsApp.SendWhatsApp (messageW.To, messageW.Text);
         }
 
