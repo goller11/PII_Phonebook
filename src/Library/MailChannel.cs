d3fd796349e8bec0c55628198e49ec857be24c3c
Vogel <vogel@github.com>
2018-10-04 16:21:03 -0300

    Commits

--- src/Library/MailChannel.cs
+++ src/Library/MailChannel.cs
@@ -3,7 +3,7 @@ using MailSenderApiUCU;
 
 namespace Library {
     public class MailChannel : IMessageChannel {
-        MailSender mailSender = new MailSender ("@", ""); // Agregar correo y contraseña
+        MailSender mailSender = new MailSender ("", ""); // Agregar correo y contraseña
         public void Send (Message message) {
             mailSender.EnviarMail (message.From, "Mensaje de difusión", message.To, "Texto alternativo", message.Text);
         }
